31f2dbc1126057cf3980b4e29bc273ad8483e8c3
Alvin Manoj Alex <alvin@Alvins-MacBook-Pro.local>
2024-07-05 16:49:50 +0530

    Code: Add navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return(
+        <div className='navbar'>
+            <div>Money Monitor</div>
+            <div>Analysis</div>
+        </div>
+    )
+}
+
+export default Navbar
