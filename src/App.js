4f863efb8a28549483291e34d0e1b394dee6b693
Alvin Manoj Alex <alvin@Alvins-MacBook-Pro.local>
2024-07-07 19:16:09 +0530

    Update: Landing page with input component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Navbar from './components/Navbar'
+import Input from './components/Input'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Navbar rightText='Analysis'/>
+      <div className='center-div'>
+        <p className='center-content'>Monitor and Analyze your expenses efficiently</p>
+      </div>
+        <Input placeholder='Type of expense' />
+        <Input placeholder='Amount spent'/>
+        <button className='submit-button'>Add Expense</button>
     </div>
   );
 }
